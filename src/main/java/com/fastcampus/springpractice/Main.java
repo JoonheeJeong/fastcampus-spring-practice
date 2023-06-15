c5b934e64732c127efce38c194c144960dd29c62
Joonhee Jeong <jeonggoo75@gmail.com>
2023-06-15 15:14:24 +0900

    Apply #1 - spring
    직접 정렬 구현체를 생성하지 않고, 스프링을 통하여 의존성을 주입하도록 각각의 구현체를 Component로 등록하고 Main을 수정합니다.

--- src/main/java/com/fastcampus/springpractice/Main.java
+++ src/main/java/com/fastcampus/springpractice/Main.java
@@ -1,14 +1,19 @@
 package com.fastcampus.springpractice;
 
+import com.fastcampus.springpractice.config.Config;
 import com.fastcampus.springpractice.logic.JavaSort;
 import com.fastcampus.springpractice.logic.Sort;
+import com.fastcampus.springpractice.service.SortService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Sort<String> sort = new JavaSort<>();
-        List<String> sorted = sort.sort(List.of(args));
+        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        SortService sortService = context.getBean(SortService.class);
+        List<String> sorted = sortService.doSort(List.of(args));
         System.out.println("[result]: " + sorted);
     }
 }
